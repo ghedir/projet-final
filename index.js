2944a7ffdfd7f353e41a11e985771e0c7100bc58
ghedir <ghedirgahada77@gmail.com>
2021-11-08 20:33:19 +0100

    make it good

--- index.js
+++ index.js
@@ -87,9 +87,8 @@ app.use("/api/categories", categoryRoute);
 
 // setup for deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('public'));
-
-  //app.use(express.static("client/build"));
+  
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
