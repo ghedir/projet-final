9692298d4d971fb95cd9d931cefb36341e1f6147
ghedir <ghedirgahada77@gmail.com>
2021-11-09 18:17:47 +0100

    commit my final projectv5

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use("/images", express.static(path.join(__dirname, "/images")));
 const PORT = process.env.PORT || 8080;
 //connecting to database
 mongoose
-  .connect(process.env.MONGO_URI , {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -87,9 +87,9 @@ app.use("/api/categories", categoryRoute);
 
 // setup for deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build/static/css/main.css"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client","build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
@@ -97,7 +97,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT , () => {
+app.listen(PORT, () => {
   console.log(`listen to the port ${PORT}`);
 });
 // module.exports = router;
