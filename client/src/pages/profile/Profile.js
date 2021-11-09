2725b4c1124c5c9a2d61944e09fa53e2cbb02529
ghedir <ghedirgahada77@gmail.com>
2021-11-09 18:26:38 +0100

    commit my final projectv7

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -5,7 +5,7 @@ import { Context } from "../../context/Context";
 
 export default function Profile() {
   const { user } = useContext(Context);
-  const PF = "http://localhost:5000/images/";
+  const PF = "/images/";
 
   return (
     <div className="profile">
