37acd6b0d2783bc084e86133b2ad7a5d60df6a3f
ghedir <ghedirgahada77@gmail.com>
2021-11-09 16:49:21 +0100

    commit my final projectv3

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,3 @@
 import axios from "axios";
-const url = "";
-export const fetchPosts = () => axios.get(`${url}/posts`);
-export const fetchCats = () => axios.get(`${url}/categories`);
+export const fetchPosts = () => axios.get("/posts");
+export const fetchCats = () => axios.get("/categories");
