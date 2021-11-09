47774b067d38ada93700012bfe865b3b99f7c211
ghedir <ghedirgahada77@gmail.com>
2021-11-09 23:25:00 +0100

    commit my final projectv15

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.use("/api/categories", categoryRoute);
 
 // setup for deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client\build"));
+  app.use(express.static("client/build/static"));
   app.get("*", (req, res) => {
     res.sendFile(__dirname, "client/build/index.html");
     
