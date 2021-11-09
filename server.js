60fd5501f9fa2325774e18399d81cd74c6db5356
ghedir <ghedirgahada77@gmail.com>
2021-11-10 00:18:49 +0100

    commit my final projectv20

--- server.js
+++ server.js
@@ -89,8 +89,8 @@ app.use("/api/categories", categoryRoute);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static('client/build'));
  
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname+ 'client/build/index.html'));
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
     
   });
 } else {
