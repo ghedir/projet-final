b09a0696d0c1b1c93aac0c9f67aa96aedb20c453
ghedir <ghedirgahada77@gmail.com>
2021-11-10 13:38:33 +0100

    make it

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 // setup for deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname + 'client/build'));
+  app.use(express.static('client/build'));
  
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + 'client/build/index.html'));
